a8a6bdbd9345ce7ec4ba0a1fad0c416b33ce5dfc
Rafael Simionato <rafaasimi@hotmail.com>
2022-02-17 20:47:26 -0300

    Criado estrutura e estilo do footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,9 +1,27 @@
+import { FaLinkedin, FaGithub, FaGlobeAmericas } from "react-icons/fa";
+import styles from "./Footer.module.css";
+
 function Footer() {
-    return (
-        <footer>
-            Meu footer
-        </footer>
-    )
+  return (
+    <footer className={styles.footer}>
+        
+      <div className={styles.socials}>
+        <a href="https://rafaelsimionato.dev/" target="_new">
+          <FaGlobeAmericas />
+        </a>
+        <a href="https://www.linkedin.com/in/rafaelzorzenon/" target="_new">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/rafaasimi" target="_new">
+          <FaGithub />
+        </a>
+      </div>
+
+      <div>
+        <p>Desenvolvido por: Rafael Simionato</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
