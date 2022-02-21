ed558dd302138faeb0de57bf61027e8c3ad49f3c
Rafael Simionato <rafaasimi@hotmail.com>
2022-02-21 17:51:39 -0300

    Criado estrutura da home e estilização

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
+import styles from './Home.module.css'
+import dashboardImg from '../assets/dashboard.svg'
+import LinkButton from '../components/layout/LinkButton/LinkButton'
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <section className={styles.homeContainer}>
+                <h1>Bem-vindo ao <span>Costs</span></h1>
+                <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+                <LinkButton to="/new-project" text="Criar Projeto" />
+                <img src={dashboardImg} alt="Costs" />
+            </section>
+        </>
     )
 }
 
