f24f71d280d985fe0c09fb5ce875271dfefd6eb7
Rafael Simionato <rafaasimi@hotmail.com>
2022-02-21 20:49:23 -0300

    Criado e estilizado componente de input

---
+++ src/components/form/Input.jsx
@@ -0,0 +1,20 @@
+import styles from "./Input.module.css";
+
+function Input({ type, text, name, placeholder, handleOnChange, value }) {
+  return (
+    <div className={styles.formControl}>
+      <label htmlFor={name}>{text}:</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        onChange={handleOnChange}
+        value={value}
+        autoComplete="off"
+      />
+    </div>
+  );
+}
+
+export default Input;
