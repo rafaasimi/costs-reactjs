435ec79d87a3760b6ac18dee1899dc5a695dbec8
Rafael Simionato <rafaasimi@hotmail.com>
2022-03-01 10:04:49 -0300

    Criação e estilização da pagina de listagem de projetos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,62 @@
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Message from "../components/layout/Message/Message";
+import Container from "../components/layout/Container/Container";
+import LinkButton from "../components/layout/LinkButton/LinkButton";
+import ProjectCard from "../components/project/ProjectCard";
+
+import styles from "./Projects.module.css";
+
 function Projects() {
-    return (
-        <h1>Projects</h1>
-    )
+  const [projects, setProjects] = useState([]);
+
+  const location = useLocation();
+  let message = "";
+
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  useEffect(() => {
+
+    fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        setProjects(data)
+    })
+    .catch((error) => console.log(error))
+
+}, [])
+
+  return (
+    <div className={styles.projectContainer}>
+      <div className={styles.titleContainer}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/new-project" text="Criar Projeto" />
+      </div>
+      {message && <Message message={message} type="success" />}
+      <Container customClass="start">
+        {projects.length > 0 && 
+            projects.map((project) => (
+                <ProjectCard
+                    id={project.id}
+                    name={project.name}
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                />
+            ))
+        }
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
