5921becbe704848c0555ccdff35884fee39e76c8
Rafael Simionato <rafaasimi@hotmail.com>
2022-03-07 19:21:23 -0300

    Criado e estilizado pagina de edição de projetos

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,10 +1,18 @@
 import styles from "./Project.module.css";
 
+import Loading from "../components/layout/Loading/Loading";
+import Container from "../components/layout/Container/Container";
+import ProjectForm from "../components/form/ProjectForm";
+import Message from "../components/layout/Message/Message";
+
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function Project() {
   const [project, setProject] = useState([]);
+  const [showProjectForm, setShowProjectForm] = useState(false);
+  const [message, setMessage] = useState();
+  const [type, setType] = useState();
 
   const { id } = useParams();
 
@@ -22,9 +30,72 @@ function Project() {
       .catch((error) => console.log(error));
   }, [id]);
 
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
+  function editPost(project) {
+    if (project.budget < project.cost) {
+      setMessage("O orçamento não pode ser menor que o custo do projeto!");
+      setType("error");
+      return false;
+    }
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProject(data);
+        setShowProjectForm(false);
+        setMessage("Projeto atualizado!");
+        setType("success");
+      })
+      .catch((error) => console.log(error));
+  }
+
   return (
     <>
-      <p>{project.name}</p>
+      {project.name ? (
+        <div className={styles.projectDetails}>
+          <Container customClass="column">
+            {message && <Message type={type} message={message} />}
+            <div className={styles.detailsContainer}>
+              <h1>Projeto: {project.name}</h1>
+              <button onClick={toggleProjectForm} className={styles.btn}>
+                {!showProjectForm ? "Editar Projeto" : "Fechar Projeto"}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.projectInfo}>
+                  <p>
+                    <span>Categoria:</span> {project.category.name}
+                  </p>
+                  <p>
+                    <span>Total de Orçamento:</span> R$ {project.budget}
+                  </p>
+                  <p>
+                    <span>Total Utilizado:</span> R$ {project.cost}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.projectInfo}>
+                  <ProjectForm
+                    handleSubmit={editPost}
+                    buttonText="Concluir Edição"
+                    projectData={project}
+                  />
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 }
